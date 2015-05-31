7ddd471b22f22dd9a0da948fc476ae8aa959743a
Neson <neson@dex.tw>
2015-05-31 13:24:05 +0800

    read request parameters

--- app.rb
+++ app.rb
@@ -5,9 +5,17 @@ Dir[File.dirname(__FILE__) + '/crawler/*.rb'].each { |file| require file }
 
 class CrawlWorker < WebTaskRunner::TaskWorker
   def exec
+    year = (Time.now.month.between?(1, 7) ? Time.now.year - 1 : Time.now.year)
+    term = (Time.now.month.between?(2, 7) ? 2 : 1)
+
+    year = params[:year].to_i if params[:year]
+    term = params[:term].to_i if params[:term]
+
+    puts "Starting crawler for #{year}-#{term} ..."
+
     crawler = NtustCourseCrawler.new(
-      year: 2015,
-      term: 1,
+      year: year,
+      term: term,
       update_progress: proc { |payload| WebTaskRunner.job_1_progress = payload[:progress] },
       after_each: proc do |payload|
         course = payload[:course]
